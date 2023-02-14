955d81304fb64703dc3f34094452a880c9a771a7
Porrapat <iporrapat@gmail.com>
2023-02-14 16:15:48 +0700

    lab18_1.cpp

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 	int nA,nB;
-	int *A,*B,*C;	
+	int *A,*B,**C;	
 	
 	cout << "Length of A: ";
 	cin >> nA;
@@ -19,29 +20,35 @@ int main(){
 	cout << "Input Array B: ";
 	for(int i=0; i < nB; i++) cin >> B[i];
 	
-	C = new int;
+	C = new int *[nA];
 	for(int i=0; i < nA; i++) C[i] = new int;
 	
-	for(int i=0; i < nA; i++){
+	for(int i=0; i < nA; i++)
+	{
 		for(int j=0; j < nB; j++) {
-			C = A[i]*B[j];
+			C[i][j] = A[i]*B[j];
 		}
 	}
 	
 	cout << "\t";
-	for(int i=0; i < nB; i++){
+	for(int i=0; i < nB; i++)
+	{
 		cout << B[i] << "\t";
 	}
 	cout << "\n-----------------------------\n";
 	
-	for(int i=0; i < nA; i++){
+	for(int i=0; i < nA; i++)
+	{
 		cout << A[i] << "\t";
 		for(int j=0; j < nB; j++) {
 			cout << C[i][j] << "\t";
 		}
 		cout << "\n";
 	}
-     delete A,B,C; 
-
+    delete [] A;
+	delete [] B;
+	for(int i=0;i<nA;i++) delete [] C[i]; 
+	delete [] C;
+	
 	return 0;
 }
